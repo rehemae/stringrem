4d49903f1bc4f2bac7084bfd92391687b7f79c7f
rehemae <rehemaephraimnanda@gmail.com>
2022-03-01 22:53:26 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,33 @@
+fun main(){
+
+
+var name= "AkiraChix"
+var x= name[0].toString()+ name[2]+ name[3]
+    println(x)
+
+    var t=parameter("rehema",30)
+    println(t)
+
+    var s=word("Zawadi")
+    println(s.count())
+
+    var m=identify("Ephraim")
+
+}
+fun parameter(x:String, y: Int):String{
+    var r=("Hi,my name is $x and i am $y years old")
+    return r
+
+
+}
+fun word(name: String):String{
+    var name="Zawadi"
+    return name
+}
+fun identify(name: String){
+    if(name=="Ephraim")
+        println("That is me")
+
+    else
+        println("I don't know who that is")
+}
